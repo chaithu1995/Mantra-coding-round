40eba083175f06e350000b9eb02594e16ca13f04
chaithu1995 <55779856+chaithu1995@users.noreply.github.com>
2021-04-21 14:15:50 +0530

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,34 @@
+import java.io.*;
+ 
+class GFG {
+        static void printArray(int arr[], int n)
+    {
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+     static void RearrangePosNeg(int arr[], int n)
+    {
+        int key, j;
+        for (int i = 1; i < n; i++) {
+            key = arr[i];
+             if (key > 0)
+                continue;
+             j = i - 1;
+            while (j >= 0 && arr[j] > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+ 
+            // Put negative element at its right position
+            arr[j + 1] = key;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = { -12, 11, -13, -5, 6, -7, 5, -3, -6 };
+        int n = arr.length;
+        RearrangePosNeg(arr, n);
+        printArray(arr, n);
+    }
+}
